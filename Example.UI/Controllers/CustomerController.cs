748312be05c97bde7b7a077315b792ff10db5f06
yarin <yarinshaer@gmail.com>
2021-07-27 10:39:15 +0300

    SDFD

--- Example.UI/Controllers/CustomerController.cs
+++ Example.UI/Controllers/CustomerController.cs
@@ -9,11 +9,14 @@ using System.Web.Http;
 
 namespace Example.UI.Controllers
 {
+    [AllowCrossSiteJson]
     public class CustomerController : ApiController
     {
+        
+
         [System.Web.Http.HttpGet]
         public IEnumerable<CustomerBO> GetCustomersWaiting()
-        {
+        { 
             try
             {
                 CustomerBL cbl = new CustomerBL();
@@ -29,12 +32,29 @@ namespace Example.UI.Controllers
         [System.Web.Http.HttpPost]
         public string CreateNewCustomer(string name)
         {
-            if (name.Length < 1)
+            if (string.IsNullOrEmpty(name)||name=="undefined")
                 return "not good";
             try
             {
                 CustomerBL cbl = new CustomerBL();
                 var listCustomers = cbl.CreateNewCustomer(name);
+                return listCustomers;
+            }
+            catch
+            {
+                return "not good";
+            }
+        }
+        [System.Web.Http.HttpPost]
+
+        public string CreateNewCustomerS(string username,string password)
+        {
+            if (string.IsNullOrEmpty(username) || username == "undefined")
+                return "not good";
+            try
+            {
+                CustomerBL cbl = new CustomerBL();
+                var listCustomers = cbl.CreateNewCustomer(username);
                 return listCustomers;
             }
             catch
